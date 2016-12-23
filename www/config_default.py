447ea73668c12f5031df3775fa1d60fa50253023
Tony Luo <luodongyi_career@163.com>
2016-12-23 11:20:47 +0800

    add day 6's configs files

---
+++ www/config_default.py
@@ -0,0 +1,17 @@
+#!usr/bin/env python3
+#-*- coding: utf-8 -*-
+
+__author__ = 'Tony Luo'
+
+configs = {
+    'db': {
+        'host': '127.0.0.1',
+        'port': 3306,
+        'user': 'root',
+        'password': 'password',
+        'database': 'awesome'
+    },
+    'session': {
+        'secret': 'AwEsOmE'
+    }
+}
