cfec3d7cb5ae8feb52cc63558216e9e487ad2dcc
Tony Luo <luodongyi_career@163.com>
2016-12-23 11:32:01 +0800

    remove unused @get method

--- www/app.py
+++ www/app.py
@@ -106,13 +106,6 @@ def datetime_filter(t):
     dt = datetime.fromtimestamp(t)
     return u'%s年%s月%s日' % (dt.year, dt.month, dt.day)
 
-@get('/')
-def index(request):
-    users = yield from User.findAll()
-    return {
-        '__template__': 'test.html',
-        'users': users
-    }
 
 async def init(loop):
     await orm.create_pool(loop=loop, host='127.0.0.1', port=3306, user='root', password='password', db='awesome')
