ce29d1d26b2ce983db02e9ae9905e4bf692d9e02
Tony Luo <luodongyi_career@163.com>
2016-12-23 17:49:54 +0800

    add auth_factory to middleware

--- www/app.py
+++ www/app.py
@@ -127,7 +127,7 @@ def datetime_filter(t):
 async def init(loop):
     await orm.create_pool(loop=loop, host='127.0.0.1', port=3306, user='root', password='password', db='awesome')
     app = web.Application(loop=loop, middlewares=[
-        logger_factory, response_factory
+        logger_factory, auth_factory,response_factory
     ])
     init_jinja2(app, filters=dict(datetime=datetime_filter))
     add_routes(app, 'handlers')
